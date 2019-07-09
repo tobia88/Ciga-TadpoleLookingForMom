50d3ced25e605c0fe72e6a825d9315d286f4f914
tobia <tophiadev88@gmail.com>
2019-07-09 23:07:49 +0800

    添加了梦境效果

--- Assets/_Project/Scripts/Levels/CreditScene.cs
+++ Assets/_Project/Scripts/Levels/CreditScene.cs
@@ -7,16 +7,31 @@ public class CreditScene: BaseScn {
     public PlayableDirector director;
     public SymbolGroup symbolGroup;
 
+    [Header("Test")]
+    public Sprite testSpr;
+    public int testSprAmount;
+
     protected override void Start() {
-        var sprites =  GameData.SymbolSprites;
-        symbolGroup.Init( sprites.ToArray() );
+        if( testSprAmount <= 0 ) {
+            var sprites = GameData.SymbolSprites;
+            symbolGroup.Init( sprites.ToArray() );
+        }
+        else {
+            var testSprs = new Sprite[testSprAmount];
+
+            for( int i = 0; i < testSprAmount; i++ ) {
+                testSprs[i] = testSpr;
+            }
+
+            symbolGroup.Init( testSprs );
+        }
 
         StartCoroutine( EndRoutine() );
     }
 
     IEnumerator EndRoutine() {
         director.Play();
-        yield return new WaitUntil( () => ( director.time / director.duration) >= 0.85f );
+        yield return new WaitUntil( () => ( director.time / director.duration ) >= 0.85f );
         PhaseState = PhaseStates.End;
     }
 }
